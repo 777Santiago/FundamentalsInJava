e3055bd588d7af1871d65a010047cae657778f37
777Santiago <dsarcilam7@gmail.com>
2025-09-24 10:26:11 -0500

    Clase 1 Finalizada

---
+++ src/HelloWorldExcercises.java
@@ -0,0 +1,34 @@
+public class HelloWorldExcercises {
+    /*
+    Aplicación Clase 1
+    Ejercicios
+     */
+    public static void main(String[] args) {
+        //1. Imprime un mensaje que diga tu nombre
+        System.out.println("Daniel Arcila");
+
+        //2. Imprime dos lineas con un solon println
+        System.out.println("Daniel Santiago\nArcila Martinez");
+
+        //3. Añade un comentario sobre cada linea del programa
+        //4. Crea un comentario en varias lineas
+        //5. Imprime tu edad tu color favorito y tu ciudad
+        System.out.println("Edad: 24\nColor Favorito: Azul Cielo\nCiudad: Zipaquirá");
+
+        //6. Utiliza varios println para escribir una frase
+        System.out.println("Dando");
+        System.out.println("Un");
+        System.out.println("Repaso");
+        System.out.println("En");
+        System.out.println("Fundamentos");
+        System.out.println("De");
+        System.out.println("Java");
+
+        //7. Imprime un diseño ASCII
+        System.out.println("☺");
+
+        //8. Intenta ejecutar el programa sin el main y observa el error
+        //9. Intenta ejecutar el programa combiando el nombre de la clase
+
+    }
+}
